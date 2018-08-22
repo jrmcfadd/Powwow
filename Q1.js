681512388ea83adcd8d0db255cba4969ddbba968
Jonathan McFadden <mcfadden.jonathan10@gmail.com>
2018-08-22 14:00:01 -0400

    Update Q1.js

--- Q1.js
+++ Q1.js
@@ -5,19 +5,23 @@ var myObj = [
     {name: "item4", isExpired: false},
     {name: "item5", isExpired: false}
 ];
-var newObjt = [];
-var newObjf = [];
-var i = 0;
-while ( i < myObj.length){
-    if (myObj[i].isExpired === true){
-        newObjt[i] = myObj[i];
-        i++
-    }
-    else{
-        newObjf[i] = myObj[i];
-        i++
-    }
+console.log(myObj.sort(
+    function(a, b){
+        return a.isExpired - b.isExpired; })
+);
 
-}
-var newobj =  newObjf.concat(newObjt);
-console.log(newobj.filter(function(e){return e}));
+// var newObjt = [];
+// var newObjf = [];
+// var i = 0;
+// while (i < myObj.length){
+//     if (myObj[i].isExpired === true){
+//         newObjt[i] = myObj[i];
+//         i++
+//     }
+//     else{
+//         newObjf[i] = myObj[i];
+//         i++
+//     }
+//
+// }
+// var newobj =  newObjf.concat(newObjt);
